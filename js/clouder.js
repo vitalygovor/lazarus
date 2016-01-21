7afb5694c1287a9a31968a734ccdd3cd5170a565
Vitaly Govor <vitaly@tuta.io>
2016-01-21 08:17:45 +0300

    Group selecting #2
    Сделан контроллер для зажатия Shift при выделении блока. Нужно добить
    сбрасывание стилей с других блоков в момент повторного выделения объекта
    без Shift-а.

--- js/clouder.js
+++ js/clouder.js
@@ -14,30 +14,59 @@ var Clouder = {
                 Clouder.each(blocks, function(key, block){
                     block.className = "block";
                     block.style.cursor = "pointer";
-                    block.addEventListener('click', function(){
-                        // Выбранный элемент
-                        Clouder.events.selectBlock.buffer.SelectedBlock = this;
-                        Clouder.events.selectBlock.selected();
+                    block.setAttribute("bid", key);
+                    block.addEventListener('click', function(event){
+
+                        if(event.shiftKey){
+                            var bIDs = Clouder.buffer.selectedBlocks;
+                            var index = bIDs.indexOf(key);
+
+                            console.log(index);
+
+                            if (index > -1) {
+                                bIDs.splice(index, 1);
+                            } else {
+                                Clouder.buffer.selectedBlocks = bIDs;
+                                Clouder.buffer.selectedBlocks.push(key);
+                                Clouder.events.selectBlockStyle(this);
+                            }
+                        } else {
+                            Clouder.buffer.selectedBlocks = [];
+                            Clouder.events.selectBlockStyle(this, key);  
+                        }
+                        console.log(Clouder.buffer.selectedBlocks);
                     });
                 });
-
-            },
-            selected: function(){
-                // this.className += " selected";
-                var blocks = document.getElementsByClassName('block');
-                Clouder.each(blocks, function(key, block){
-                    block.className = "block";
-                    block.style.cursor = "pointer";
-                });
-                if(Clouder.events.selectBlock.buffer.SelectedBlock != undefined){
-                    Clouder.events.selectBlock.buffer.SelectedBlock.className += " selected";
-                }
-            },
-            buffer: {
-                SelectedBlock: undefined,
-                SelectedBlocks: undefined,
             }
+        },
+        selectBlockStyle: function(element, key){
+
+            var bIDs = Clouder.buffer.selectedBlocks;
+
+            function resetStyles(element){
+                for (var i = 1; i <= bIDs.length; i++) {
+                    console.log(i);
+                }
+            }
+
+            console.log(bIDs.length);
+
+            if(bIDs.length >= 1){
+               element.className = "block";
+               element.className += " selected";
+            } else {
+                for (var i = 0; i < bIDs.length; i++) {
+                    // var element = document.querySelectorAll( 'div[bid="'+i+'"]' );
+                    document.querySelectorAll('div[bid="'+i+'"]' ).className = "block";
+                    // element.className += " selected";   
+                }
+            }
+
+            resetStyles(element);
         }
+    },
+    buffer: {
+        selectedBlocks: [],
     },
     each: function each(data, callback){
         for(var key in data){
@@ -45,6 +74,21 @@ var Clouder = {
                 callback(key, data[key]);
             }
         }
+    },
+    getChar: function(event){
+        if (event.which == null) { // IE
+            //if (event.keyCode < 32) return null; // спец. символ
+            console.log(String.fromCharCode(event.keyCode));
+            console.log(event);
+        }
+
+        if (event.which != 0 && event.charCode != 0) { // все кроме IE
+            //if (event.which < 32) return null; // спец. символ
+            console.log(String.fromCharCode(event.which));
+            console.log(event.charCode);
+        }
+
+        return null; // спец. символ
     },
     run: function run(){
         Clouder.events.selectBlock.select();
