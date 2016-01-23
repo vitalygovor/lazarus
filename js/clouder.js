4f3aa746bd13c14999cabc7a075c92a5f1168243
Vitaly Govor <vitaly@tuta.io>
2016-01-24 02:10:49 +0300

    Group Selecting #6
    Код оптимизирован. Нужно исправить баг двойным кликом по блоку, чтобы
    после первого группового выделения можно было одним кликом выделять
    блок. Сейчас почему-то за два блока.

--- js/clouder.js
+++ js/clouder.js
@@ -1,134 +1,121 @@
 var Clouder = {
-    init: function(){
-        console.log("Initialization...");
+    BUFFER: {
+        block: {
+            select: {
+                blocks: []
+            }
+        }
     },
-    events: {
-        selectBlock: {
+    ui: {
+        block: {
             select: function(){
 
-                console.log('selectBlock');
+                // Установка переменных буфера
+                var BUFFER = Clouder.BUFFER.block.select;
+                // Массив для блоков
+                var BIDS = BUFFER.blocks;
+
+                // Добавляем нужные функции
+                var Each = Clouder.func.each;
+                var ChangeClasses = Clouder.func.ChangeClasses;
+                var WinSelectOff = Clouder.func.clearSelection;
+
                 // Выбираем все блоки с классом block
                 var BLOCKS = document.getElementsByClassName('block');
-                // Цикл по всем block
-
-                Clouder.each(BLOCKS, function(key, block){
+                
+                // Цикл по всем нужным элементам
+                Clouder.func.each(BLOCKS, function(key, block){
+                    WinSelectOff();
                     block.style.cursor = "pointer";
                     block.setAttribute("bid", key);
-
+                    // Прослушиваем действие
                     block.addEventListener('click', function(event){
-                        var bIDs = Clouder.buffer.selectedBlocks;
                         var CleckedBlock = this.getAttribute('bid');
                         
                         if(event.shiftKey){
-                           
-                            var bIDs = Clouder.buffer.selectedBlocks;
-                            var index = bIDs.indexOf(key);
-
-                            console.log(index);
-
-                            if (index > -1) {
-                                bIDs.splice(index, 1);
-                            } else {
-                                Clouder.buffer.selectedBlocks = bIDs;
-                                Clouder.buffer.selectedBlocks.push(key);
-                                Clouder.events.selectBlockStyle(this);
-                            }
+                            GroupSelect(this, key);
                         } else {
-
-                            Clouder.each(BLOCKS, function(key, block){
-                                block.className = "block";
-                                if(key == CleckedBlock) block.className += " selected";
-                            });
-
-
-                            if(bIDs.length == 0) {
-                                console.log('Не выбрано ни одного блока');
-                            } 
-
-                            Clouder.buffer.selectedBlocks = [];
-                            Clouder.events.selectBlockStyle(this, key);  
+                            resetStylesForNotSelectedBlocks();
+                            // Clouder.func.block.style(this, key);  
                         }
 
+                        function GroupSelect(block, key){
+                            var index = BIDS.indexOf(key);
 
-                        // if(bIDs.length == 0){
-                            
-                        //     var bIDofThisBlock = this.getAttribute("bid");
-                        //     var CountOfBufferSelectedBlocks = Clouder.buffer.selectedBlocks;
+                            if (index > -1) {
+                                BIDS.splice(index, 1);
+                            } else {
+                                BIDS.push(key);
+                                Clouder.ui.block.style(block, key); 
+                            }
+                        }
 
-                        //     // console.log(this.getAttribute("bid"));
-                        //     for (var i = 0; i < blocks.length; i++) {
-                        //         if(i != this.getAttribute("bid")) {
-                        //             block.className = "block";
-                        //             // console.log("i:"+i);
-
-                        //         } else if(i == this.getAttribute("bid")) {
-                        //             this.className = "block selected";
-                        //             console.log("i:"+i);
-
-                        //         }
-                        //         // var element = document.querySelectorAll( 'div[bid="'+i+'"]' );
-                        //         // document.querySelectorAll('div[bid="'+i+'"]' ).className = "block selected";
-                        //         // element.className += " selected";   
-                        //     }
-                        // } 
+                        function resetStylesForNotSelectedBlocks(key, block) {
+                            Each(BLOCKS, function(key, block){
+                                ChangeClasses(block, '', "block");
+                                if(key == CleckedBlock) ChangeClasses(block, 'selected', "block");
+                            });
+                        }
                         
-                        console.log(Clouder.buffer.selectedBlocks);
                     });
                 });
+
+                console.log(Clouder.BUFFER.block.select);
+
+            },
+            style: function(element, key){
+
+                // Установка переменных буфера
+                var BUFFER = Clouder.BUFFER.block.select;
+                // Массив для блоков
+                var BIDS = BUFFER.blocks;
+
+                if(BIDS.length > 0){
+                   element.className = "block";
+                   element.className += " selected";
+                }
+
+            }
+        }
+    },
+    func: {
+        ChangeClasses: function (block, addClass, defaultClass){
+            // block - блок, с которым манипуляции проходят
+            // addClass - название класса, который нужно добавить к блоку
+            // defaultClass - главный первый класс. Например, block
+            if(block) block.className = ""+defaultClass+" "+addClass+"";
+        },
+        each: function each(data, callback){
+            for(var key in data){
+                if(data.hasOwnProperty(key)){
+                    callback(key, data[key]);
+                }
             }
         },
-        selectBlockStyle: function(element, key){
-
-            var bIDs = Clouder.buffer.selectedBlocks;
-
-            function resetStyles(element){
-                for (var i = 0; i <= bIDs.length; i++) {
-                    console.log(i);
-                }
-            }
-            console.log(bIDs.length);
-
-            if(bIDs.length > 0){
-               element.className = "block";
-               element.className += " selected";
-            } else {
-                for (var i = 0; i < bIDs.length; i++) {
-                    // var element = document.querySelectorAll( 'div[bid="'+i+'"]' );
-                    document.querySelectorAll('div[bid="'+i+'"]' ).className = "block";
-                    // element.className += " selected";   
-                }
+        getChar: function(event){
+            if (event.which == null) { // IE
+                //if (event.keyCode < 32) return null; // спец. символ
+                console.log(String.fromCharCode(event.keyCode));
+                console.log(event);
             }
 
-            resetStyles(element);
-        }
-    },
-    buffer: {
-        selectedBlocks: [],
-        selectBlock: [],
-    },
-    each: function each(data, callback){
-        for(var key in data){
-            if(data.hasOwnProperty(key)){
-                callback(key, data[key]);
+            if (event.which != 0 && event.charCode != 0) { // все кроме IE
+                //if (event.which < 32) return null; // спец. символ
+                console.log(String.fromCharCode(event.which));
+                console.log(event.charCode);
+            }
+
+            return null; // спец. символ
+        },
+        clearSelection: function clearSelection() {
+            if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+            } else { // старый IE
+                document.selection.empty();
             }
         }
-    },
-    getChar: function(event){
-        if (event.which == null) { // IE
-            //if (event.keyCode < 32) return null; // спец. символ
-            console.log(String.fromCharCode(event.keyCode));
-            console.log(event);
-        }
-
-        if (event.which != 0 && event.charCode != 0) { // все кроме IE
-            //if (event.which < 32) return null; // спец. символ
-            console.log(String.fromCharCode(event.which));
-            console.log(event.charCode);
-        }
-
-        return null; // спец. символ
     },
     run: function run(){
-        Clouder.events.selectBlock.select();
+        Clouder.ui.block.select();
     }
 };
